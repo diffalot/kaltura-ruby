a7303ba3120365025742b9bb5bcde32d3b6f691f
jbcplatter <patricksrobertson@gmail.com>
2010-06-30 11:08:48 -0400

    starting namespace organization

--- lib/kaltura-ruby.rb
+++
@@ -1,5 +0,0 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'kaltura_client'
-require 'yaml'
-
-include Kaltura
---
+++ lib/kaltura.rb
@@ -0,0 +1,10 @@
+#require 'kaltura_client'
+require 'kaltura/kaltura_client_base'
+require 'kaltura/kaltura_client'
+require 'yaml'
+
+#include Kaltura
+
+module Kaltura
+
+end
